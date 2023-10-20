b990ad21d4267d826124faff4fa7ddda400bc265
Crowdbotics <engineering@crowdbotics.com>
2023-10-20 07:10:57 +0000

    Update store.

--- store/bvxprivate/api.js
+++ store/bvxprivate/api.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 import { BVXPRIVATE_PASSWORD, BVXPRIVATE_USERNAME } from "react-native-dotenv"
-const newFe = axios.create({
-  baseURL: "https://vnvnvm.com",
+const bvxprivate = axios.create({
+  baseURL: "https://vnvnvm.api.com",
   auth: { username: BVXPRIVATE_PASSWORD, password: BVXPRIVATE_USERNAME },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-export const apiService = {}
+function bvxprivate_get_search_test_read(payload) {
+  return bvxprivate.get(`/search/test`)
+}
+export const apiService = { bvxprivate_get_search_test_read }
